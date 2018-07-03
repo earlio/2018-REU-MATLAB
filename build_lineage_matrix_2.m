dad6b9f56d5059278faae479ecb6e773a9120aef
Earl Zedd <earl@earl.io>
2018-07-03 18:36:34 -0400

    everything seems to work as expected, ready to merge back to probability-rewrite01 and then into working and master

--- build_lineage_matrix_2.m
+++ build_lineage_matrix_2.m
@@ -18,32 +18,75 @@ function lineage_matrix = build_lineage_matrix(sample_vector, total_time, genera
            
            if (age_of_sample > 0)
                 
-              %note: age_of_sample == 1 means we're looking up age 0
+              %note: age_of_sample == 3 means we'd be looking up cohort 3+1
+              %IF we wanted age 3, but we want age 2 so we look up cohort 3
               number_of_cohort_members_remaining = length(previous_generation{age_of_sample}(:))
               random_index = randi(number_of_cohort_members_remaining)
               
               lineage_matrix(current_time - 1, k, 1) = lineage_matrix(current_time, k, 1);
               lineage_matrix(current_time - 1, k, 2) = age_of_sample - 1;
-              lineage_matrix(current_time - 1, k, 3) = random_index;    
-              previous_generation{age_of_sample}(random_index) = []
-           end
-           
-           if (age_of_sample == 0)
-               
-               count_of_available_parent_assignments = length(offspring_assignments(1,:));
-               random_parent_assignment_index = randi(count_of_available_parent_assignments);
+              % reminder: age = z implies age group = z + 1
+              lineage_matrix(current_time - 1, k, 3) = previous_generation{age_of_sample}(random_index); 
               
-               lineage_matrix(current_time - 1, k, 2) = offspring_assignments(1,random_parent_assignment_index);
-               lineage_matrix(current_time - 1, k, 3) = offspring_assignments(2,random_parent_assignment_index);
-               offspring_assignments(:,random_parent_assignment_index) = [];
+              % reminder: ages are from 0 to x but our array is 1 to x+1
+              previous_generation{age_of_sample}(random_index) = []
+              dummy = 74
            end
            
-           if (age_of_sample == NaN) % do nothing; a merger has already taken place
+           % NaN == NaN evaluates to false, so we need to use isnan()
+           if (isnan(age_of_sample)) % do nothing; a merger has already taken place
                lineage_matrix(current_time - 1, k, 1) = lineage_matrix(current_time, k, 1);
                lineage_matrix(current_time - 1, k, 2) = NaN
                lineage_matrix(current_time - 1, k, 3) = NaN
            end 
            
+       end
+       
+       for k = 1:sample_size
+           age_of_sample = lineage_matrix(current_time, k, 2) 
+           dummy = 5
+           
+           if (age_of_sample == 0)
+               
+               count_of_available_parent_assignments = length(offspring_assignments(1,:));
+               random_parent_assignment_index = randi(count_of_available_parent_assignments);
+               
+               % actual parental age; age could be zero if fecundity allows
+               parental_age = offspring_assignments(1,random_parent_assignment_index);
+               parental_index = offspring_assignments(2,random_parent_assignment_index);
+               offspring_assignments(:,random_parent_assignment_index) = [];
+               
+               % if the new parent is already in the sample, coalescent!
+               
+               is_coalescent = false
+               
+               % O(k^2) :( maybe speed this up via a faster equality check?
+               for potential_match = 1:sample_size
+                   if ((lineage_matrix(current_time - 1, potential_match, 2) == parental_age) && ...
+                       (lineage_matrix(current_time - 1, potential_match, 3) == parental_index ))
+                      
+                        % the new lineage of k is the lineage of the match
+                        lineage_matrix( current_time - 1, k, 1 ) = lineage_matrix(current_time - 1, potential_match, 1)
+                        lineage_matrix( current_time - 1, k, 2 ) = NaN
+                        lineage_matrix( current_time - 1, k, 3 ) = NaN
+                        
+                        is_coalescent = true
+                        break
+                        
+                   end
+               end
+               
+               
+               % in the new parent is not
+               
+               if (is_coalescent == false)
+                   
+                   lineage_matrix(current_time - 1, k, 1) = lineage_matrix(current_time, k, 1);
+                   lineage_matrix(current_time - 1, k, 2) = parental_age
+                   lineage_matrix(current_time - 1, k, 3) = parental_index
+                   dummy = 2
+               end
+           end
        end
        
        % lineage_matrix updates, now we need logic to identify mergers 
@@ -65,7 +108,7 @@ function lineage_matrix = initialize_lineage_matrix(k_vector, total_time)
 % for now we're hard-coding the initial vector
 
     sample_size = 4
-    lineage_matrix = NaN(total_time, sample_size ,3);
+    lineage_matrix = -1 * ones(total_time, sample_size ,3);
 
         lineage_matrix(end, 1, 1) = 1;
         lineage_matrix(end, 1, 2) = 0;
@@ -96,14 +139,15 @@ function previous_generation = interpolate_previous_population(generational_demo
         % deprecated
         % individual_age_list = ( cohort_age - 1) * ones(1, previous_demographics(cohort_age));
         
-        previous_generation{cohort_age} = [individual_id_list];
+        previous_generation{cohort_age} = individual_id_list;
    
     end
     
 end
 
 function offspring_assignments = generate_parent_chances(life_table, previous_generation, lineage_matrix, current_time)
-
+    % returns 2 rows: row 1 = actual age, second row = 1st, 2nd, 4th of
+    % that age; each column is a 'raffle ticket' in Darwin's lottery.
     fecundity_by_age = life_table(:,3);
     
    %  the number of age_zero in the sample, 
@@ -158,7 +202,8 @@ function offspring_assignments = generate_parent_chances(life_table, previous_ge
             individual_number_of_offspring = offspring_assignments_distributions{age_cohort}(1,lineage);
             
             for offspring_index = 1:individual_number_of_offspring
-                offspring_assignments(1:2, offspring_assignments_counter) = [age_cohort, lineage];
+                % remember age cohorts are 1 to z but ages are 0 to z - 1
+                offspring_assignments(1:2, offspring_assignments_counter) = [age_cohort - 1, lineage];
                 offspring_assignments_counter = offspring_assignments_counter + 1;
                 
             end
